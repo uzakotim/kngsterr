d5a8e2494a373e7b2f320f00e67eb98d0ec9940d
uzakotim <timauzakov@gmail.com>
2025-02-04 17:31:25 +0500

    redirect works

--- kngsterr-api/src/main.rs
+++ kngsterr-api/src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router, response::Redirect};
+use std::net::SocketAddr;
+use tokio;
+use tower_http::cors::{Any, CorsLayer};
+use dotenv::dotenv;
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let google_client_id = env::var("GOOGLE_CLIENT_ID").expect("GOOGLE_CLIENT_ID not set");
+    // let github_client_id = env::var("GITHUB_CLIENT_ID").expect("GITHUB_CLIENT_ID not set");
+    let cors = CorsLayer::new()
+    .allow_methods(Any)  // Allows all HTTP methods
+    .allow_headers(Any)  // Allows all headers
+    .allow_origin(Any);  // Allows requests from frontend
+
+    let app = Router::new()
+        .route("/api/auth/google", get(move || async move {
+            let redirect_url = format!(
+                "https://accounts.google.com/o/oauth2/auth?client_id={}&redirect_uri=http://localhost:8000/callback&response_type=code&scope=email profile",
+                google_client_id
+            );
+            // format!("Redirect to: {}", redirect_url)
+            Redirect::temporary(&redirect_url)
+        }))
+        // .route("localhost:3000/api/auth/github", get(move || async move {
+        //     let redirect_url = format!(
+        //         "https://github.com/login/oauth/authorize?client_id={}&redirect_uri=http://localhost:8000/callback&scope=user",
+        //         github_client_id
+        //     );
+        //     format!("Redirect to: {}", redirect_url)
+        // }))
+        .layer(cors);
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    println!("Listening on http://{}", addr);
+    axum::serve(listener, app).await.unwrap();
 }
--- kngsterr-ui/src/pages/login.rs
+++ kngsterr-ui/src/pages/login.rs
@@ -4,26 +4,27 @@ use gloo_console::log;
 
 #[component]
 pub fn LoginPage() -> impl IntoView {
+    let backend_url = "http://localhost:8000/api/auth/google";
     view! {
         <div class="container">
             <h1>"Login Page"</h1>
-            <button on:click=move |_| {
-                if let Some(win) = window() {
-                    match win.location().set_href("/api/auth/github") {
-                        Ok(_) => (), // Success, do nothing or log
-                        Err(err) => {
-                            // Handle the error (e.g., log it to the console)
-                            log!("Error redirecting to GitHub: {:?}", err);
-                            // Optionally display an error message to the user
-                            // alert!("Error during GitHub login."); // Requires web_sys::console::alert
-                        }
-                    }
-                }
-            }>"Login with GitHub"</button>
+            // <button on:click=move |_| {
+            //     if let Some(win) = window() {
+            //         match win.location().set_href("/api/auth/github") {
+            //             Ok(_) => (), // Success, do nothing or log
+            //             Err(err) => {
+            //                 // Handle the error (e.g., log it to the console)
+            //                 log!("Error redirecting to GitHub: {:?}", err);
+            //                 // Optionally display an error message to the user
+            //                 // alert!("Error during GitHub login."); // Requires web_sys::console::alert
+            //             }
+            //         }
+            //     }
+            // }>"Login with GitHub"</button>
 
             <button on:click=move |_| {
                 if let Some(win) = window() {
-                    match win.location().set_href("/api/auth/google") {
+                    match win.location().set_href(backend_url) {
                         Ok(_) => (),
                         Err(err) => {
                             log!("Error redirecting to Google: {:?}", err);
